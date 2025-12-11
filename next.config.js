f343800ab19ade0e8d481c31fee21c4be13443b6
notdemy <devdemy09@gmail.com>
2025-12-12 03:46:40 +0530

    Remove assetPrefix for Vercel deployment compatibility

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd ? "/flappy/" : "",
   images: {
     unoptimized: true,
   },
